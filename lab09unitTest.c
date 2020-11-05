8e822fcfb65e1018cd221d60095843fed5ed4e25
Cowie <1557271232@mil>
2020-11-05 16:19:14 -0700

    first commit

---
+++ lab09unitTest.c
@@ -0,0 +1,29 @@
+// *** lab09unitTest.c*** //
+/** labXX.c
+* ===========================================================
+* Name: Cleo Cowie, 08SEPT2020
+* Section:
+* Project: 
+* Purpose: 
+* ===========================================================
+*/
+
+/* #include <stdio.h>
+#include <assert.h>
+#include "lab09functs.h"
+#include <math.h>
+
+int main (void) {
+
+printf("Testing started\n");
+
+assert(fabs(volumeCylinder(10.5,10.5) - 3636.786196) <= 0.00001);
+assert(volumeBox(10.5,10.5,12.5)== 1378.125000);
+assert(fabs(degToRad(90) - 1.570796) <= 0.000001);
+
+printf("Testing complete\n");
+char c;
+scanf(" %c", &c);
+return 0;
+
+} */
